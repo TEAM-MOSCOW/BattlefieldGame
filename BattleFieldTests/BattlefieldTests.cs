26c6b6166d2d20161edbe35d4ff4e607986be1f4
Filip Kolev <fil.kolev@gmail.com>
2015-05-07 02:12:45 +0300

    minor fixes

--- BattleFieldTests/BattlefieldTests.cs
+++ BattleFieldTests/BattlefieldTests.cs
@@ -1,23 +1,21 @@
-﻿using System;
-using Battlefield;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace BattleFieldTests
+﻿namespace BattleFieldTests
 {
+    using System;
     using Battlefield;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class BattlefieldTests
     {
         [TestMethod]
-        [ExpectedException(typeof (ArgumentOutOfRangeException))]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void TestCreateBattlefieldWithFieldSizeOverTheMax()
         {
             Battlefield battlefield = Battlefield.Create(11);
         }
 
         [TestMethod]
-        [ExpectedException(typeof (ArgumentOutOfRangeException))]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void TestCreateBattlefieldWithNegativeFieldSize()
         {
             Battlefield battlefield = Battlefield.Create(-11);
@@ -37,7 +35,7 @@ namespace BattleFieldTests
             Cell cellInRange = new Cell(1, 1);
             Cell cellCoordinatesOverFieldSize = new Cell(4, 4);
             Cell cellNegativeCoordinates = new Cell(-1, -1);
-            Assert.IsTrue(battlefield.IsCellInRange(cellInRange), "Cell shoud be in range");
+            Assert.IsTrue(battlefield.IsCellInRange(cellInRange), "Cell should be in range");
             Assert.IsFalse(battlefield.IsCellInRange(cellCoordinatesOverFieldSize), "Cell should not be in range");
             Assert.IsFalse(battlefield.IsCellInRange(cellNegativeCoordinates), "Cell should not be in range");
         }
